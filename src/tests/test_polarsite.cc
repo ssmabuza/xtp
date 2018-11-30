86ecbcb09ae94c098f215f483a3778478afff4a1
Tirimbo, G <g.tirimbo@tue.nl>
2018-11-30 14:48:26 +0100

    added more unit tests to polarSite

--- src/tests/test_polarsite.cc
+++ src/tests/test_polarsite.cc
@@ -90,7 +90,7 @@ if(!equalmultipoles){
 BOOST_AUTO_TEST_CASE(interaction_test) {
   PolarSite ps1(1,"ps1");
   PolarSite ps2(2,"ps2",Eigen::Vector3d::UnitX());
-  
+  //Charge-Charge Static Interaction Test
   Eigen::VectorXd mp1 = Eigen::VectorXd::Zero(1);
   Eigen::VectorXd mp2 = Eigen::VectorXd::Zero(1);
   mp1<<1;
@@ -100,19 +100,19 @@ BOOST_AUTO_TEST_CASE(interaction_test) {
   ps1.setMultipole(mp1);
   ps2.setMultipole(mp2);
   
-  double Energyref=-1;
-  double Energy= ps1.InteractStatic(ps2);
-   BOOST_CHECK_EQUAL(std::abs(Energy-Energyref)<1e-9,true); 
-   
+  double Energyref1=-1;
+  double Energy1= ps1.InteractStatic(ps2);
+  BOOST_CHECK_EQUAL(std::abs(Energy1-Energyref1)<1e-9,true); 
+   // Electric Field onto polarsite test
   bool check_field=ps1.getField().isApprox(ps2.getField(),1e-5);
   if(!check_field){
     std::cout<<"Field at ps1"<<std::endl;
     std::cout<<ps1.getField()<<std::endl;
-     std::cout<<"Field at ps2"<<std::endl;
+    std::cout<<"Field at ps2"<<std::endl;
     std::cout<<ps2.getField()<<std::endl;
   }
   BOOST_CHECK_EQUAL(check_field,true);
-  
+  // Potential onto polarsite test
   bool check_potential=std::abs(ps1.getPotential()+ps2.getPotential())<1e-5;
   if(!check_potential){
      std::cout<<"Potential at ps1"<<std::endl;
@@ -120,7 +120,9 @@ BOOST_AUTO_TEST_CASE(interaction_test) {
      std::cout<<"Potential at ps2"<<std::endl;
     std::cout<<ps2.getPotential()<<std::endl;
   }
-      BOOST_CHECK_EQUAL(check_potential,true);
+  BOOST_CHECK_EQUAL(check_potential,true);
+  
+  /*    
   PolarSite ps3(3,"ps3");
   PolarSite ps4(4,"ps4",Eigen::Vector3d::UnitZ());
   Eigen::VectorXd multipole=Eigen::VectorXd::Zero(9);
@@ -131,6 +133,74 @@ BOOST_AUTO_TEST_CASE(interaction_test) {
   ps3.setMultipole(multipole);
   ps4.setMultipole(multipole);
   ps3.InteractStatic(ps4);
+  */
+  
+  //Dipole-Dipole Interaction Test
+  PolarSite ps5(5,"ps5");
+  PolarSite ps6(6,"ps6",Eigen::Vector3d::UnitZ());
+  Eigen::VectorXd dipole1=Eigen::VectorXd::Zero(4);
+  dipole1<<0,0,0,1;  
+  
+  ps5.setPolarisable(false);
+  ps6.setPolarisable(false);
+  ps5.setMultipole(dipole1);
+  ps6.setMultipole(-dipole1);
+  //I put two dipoles anti-parellel to z (pag.52 Stone's book)
+  
+  double Energyref2 = 2;
+  double Energy2 = ps5.InteractStatic(ps6);
+  bool check_dipole_dipole = std::abs(Energy2-Energyref2)<1e-9;
+  if(!check_dipole_dipole){
+    std::cout<<"Dipole1 at"<<std::endl;
+    std::cout<<ps5.getPos()<<std::endl;
+    std::cout<<"Dipole2 at"<<std::endl;
+    std::cout<<ps6.getPos()<<std::endl;
+    std::cout<<"Energy"<<std::endl;
+    std::cout<<Energy2<<std::endl;
+  }
+  BOOST_CHECK_EQUAL(std::abs(Energy2-Energyref2)<1e-9,true); 
+  
+  
+  //quadrupole-quadrupole Interaction Test
+  PolarSite ps7(7,"ps7");
+  PolarSite ps8(8,"ps8",Eigen::Vector3d::UnitZ());
+  Eigen::VectorXd multipole1=Eigen::VectorXd::Zero(9);
+  multipole1<<0,0,0,0,1,0,0,0,0;
+  
+  ps7.setPolarisable(false);
+  ps8.setPolarisable(false);
+  ps7.setMultipole(multipole1);
+  ps8.setMultipole(multipole1);
+  //I put two quadrupoles on the z axis (see pag. 53 Stone's Book)
+  double Energy3 = ps7.InteractStatic(ps8);
+  double Energyref3 = 6;
+  bool check_quadrupole_quadrupole = std::abs(Energy3-Energyref3)<1e-9;
+  if(!check_quadrupole_quadrupole){
+      std::cout<<"Quadrupole-Quadrupole interaction energy="<<Energy3<<std::endl;
+  }
+  BOOST_CHECK_EQUAL(check_quadrupole_quadrupole,true); 
+  
+  
+  //dipole-quadrupole Interaction Test
+  PolarSite ps9(9,"ps9");
+  PolarSite ps10(8,"ps10",Eigen::Vector3d::UnitZ());
+  Eigen::VectorXd dipole2=Eigen::VectorXd::Zero(4);
+  dipole2<<0,0,0,1;
+  Eigen::VectorXd multipole2=Eigen::VectorXd::Zero(9);
+  multipole2<<0,0,0,0,1,0,0,0,0;
+  
+  ps9.setPolarisable(false);
+  ps10.setPolarisable(false);
+  ps10.setMultipole(dipole2);
+  ps9.setMultipole(multipole2);
+  //I put one quadrupole in the origin and one dipole on the z axis parallel to this (pag.55 Stone's book)
+  double Energy4 = ps9.InteractStatic(ps10);
+  double Energyref4 = -3;
+  bool check_dipole_quadrupole = std::abs(Energy4-Energyref4)<1e-9;
+  if(!check_dipole_quadrupole){
+      std::cout<<"Dipole-Quadrupole interaction energy="<<Energy3<<std::endl;
+  }
+  BOOST_CHECK_EQUAL(check_dipole_quadrupole,true);    
 }
 
 
@@ -138,25 +208,41 @@ BOOST_AUTO_TEST_CASE(induction_test) {
   PolarSite ps1(1,"ps1");
   PolarSite ps2(2,"ps2",Eigen::Vector3d::UnitX());
   
-  Eigen::VectorXd mp1 = Eigen::VectorXd::Zero(1);
-  Eigen::VectorXd mp2 = Eigen::VectorXd::Zero(1);
-  mp1<<1;
-  mp2<<-1;
+  Eigen::VectorXd mp1 = Eigen::VectorXd::Zero(4);
+  Eigen::VectorXd mp2 = Eigen::VectorXd::Zero(4);
+  mp1<<1,0,0,0;
+  mp2<<1,0,0,0;
   ps1.setPolarisable(true);
   ps2.setPolarisable(true);
   ps1.setMultipole(mp1);
   ps2.setMultipole(mp2);
+  
+  std::cout<<"Site 1:"<<std::endl;
+  std::cout<<ps1.getDipole()<<std::endl;
+  std::cout<<"Site 2:"<<std::endl;
+  std::cout<<ps2.getDipole()<<std::endl;
+  
   Eigen::Matrix3d poltensor=Eigen::Matrix3d::Zero();
+  /*
   poltensor<<2,1,0,
             1,3,1,
             0,1,2.5;
-  
+  */
+  poltensor<< 1,0,0,
+                0,1,0,
+                  0,0,1;
   ps1.setPolarisation(poltensor);
   ps2.setPolarisation(poltensor);
   
   double Energy= ps1.InteractStatic(ps2);
   ps1.Induce(1);
   ps2.Induce(1);
+  std::cout<<"Site 1:"<<std::endl;
+  std::cout<<ps1.getDipole()<<std::endl;
+  std::cout<<"Site 2:"<<std::endl;
+  std::cout<<ps2.getDipole()<<std::endl;
+  
+  
   double alpha=0.39;
   double InductionEnergy=ps1.InteractInduction(ps2,alpha);
  
